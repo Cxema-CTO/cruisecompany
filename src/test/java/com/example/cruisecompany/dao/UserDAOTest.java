8579e23f6f71538abbc5d801db1bd3ec6e8d10f3
Andriy <andrii.aff@gmail.com>
2022-12-29 02:27:52 +0100

    next commit

--- src/test/java/com/example/cruisecompany/dao/UserDAOTest.java
+++ src/test/java/com/example/cruisecompany/dao/UserDAOTest.java
@@ -1,23 +1,34 @@
 package com.example.cruisecompany.dao;
 
 import com.example.cruisecompany.entity.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mockStatic;
 
 class UserDAOTest {
 
-//    static UserDAO dao;
-//
-//    @BeforeEach
-//    public void init(){
-//        dao = new UserDAO();
-//    }
-
     @Test
     void howMuchIsTheFish() {
+        try (MockedStatic<UserDAO> dao = mockStatic(UserDAO.class)) {
+            dao.when(UserDAO::howMuchIsTheFish).thenReturn(222);
+            int users = UserDAO.howMuchIsTheFish();
+            System.out.println(users);
+            assertEquals(222, users);
+        }
     }
+
+    @Test
+    void howMuchIsTheFishWithoutTry() {
+        MockedStatic<UserDAO> dao = mockStatic(UserDAO.class);
+        dao.when(UserDAO::howMuchIsTheFish).thenReturn(777);
+        int users = UserDAO.howMuchIsTheFish();
+        System.out.println(users);
+        assertEquals(777, users);
+    }
+
 
     @Test
     void paginationUsers() {
@@ -57,9 +68,14 @@ class UserDAOTest {
 
     @Test
     void getUserFromDB() {
-        User testUser = new User(1,"Admin","21232f297a57a5a743894a0e4a801fc3",true,false,100);
-
+        User testUser = new User(1, "Admin", "21232f297a57a5a743894a0e4a801fc3", true, false, 100);
         User user = UserDAO.getUserFromDB("Admin");
-        assertEquals(testUser.hashCode(),user.hashCode());
+        assertEquals(testUser.hashCode(), user.hashCode());
+    }
+
+    @Test
+    void getUserFromDBisNull() {
+        User user = UserDAO.getUserFromDB("superAdmin");
+        assertNull(user);
     }
 }
